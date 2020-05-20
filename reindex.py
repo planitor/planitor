b2a071c51cda71f35bb969edb7b293960b3a937d
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-05-20 22:43:52 +0000

    mobile layout for minute

--- reindex.py
+++ reindex.py
@@ -5,23 +5,27 @@ from planitor.database import db_context
 from planitor.models import Minute, Meeting
 
 
-def main(last: int = 0, force: bool = False):
+def main(last: int = 0, force: bool = False, worker: bool = False):
     """ Reindex minutes from latest to oldest. """
 
     with db_context() as db:
         query = db.query(Minute).join(Meeting)
 
         if not force:
-            query = query.filter(Minute.lemmas != None)
+            query = query.filter(Minute.lemmas == None)  # noqa
 
         query = query.order_by(Meeting.start.desc())
 
         if last > 0:
             query = query.limit(last)
 
+        func = update_minute_with_lemmas
+        if worker:
+            func = func.send
+
         for minute in query:
             try:
-                lemmas = update_minute_with_lemmas(minute.id, force=force, db=db) or ""
+                lemmas = func(minute.id, force=force, db=db) or ""
             except KeyboardInterrupt:
                 print("^C")
                 break
