20a89caf91e56a0c1837687e44b12b28aacc1ca7
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-07-08 16:33:14 +0000

    More memory efficient PDF thumbnails

--- planitor/attachments.py
+++ planitor/attachments.py
@@ -1,3 +1,4 @@
+import ctypes
 from contextlib import contextmanager
 from io import BytesIO
 from typing import Iterator, Tuple
@@ -5,13 +6,19 @@ from urllib.request import urlopen
 
 import boto3
 import dramatiq
-
-from wand.image import Image
+from sqlalchemy.orm import Session
+from wand.compat import binary
+from wand.api import library, libmagick
 from wand.color import Color
+from wand.image import Image
+from wand.sequence import SingleImage
 
-from . import config, ENV
+from . import ENV, config
 from .database import db_context
 from .models import Attachment, AttachmentThumbnail
+
+library.MagickNextImage.argtypes = [ctypes.c_void_p]
+library.MagickNextImage.restype = ctypes.c_int
 
 s3 = boto3.client(
     "s3",
@@ -29,21 +36,67 @@ def upload(bytes_io_file: BytesIO, key: str) -> None:
 
 
 @contextmanager
-def get_image(url: str):
+def get_pdf_image(url: str):
     with urlopen(url) as resource:
-        with Image(file=resource, resolution=288) as image:
-            yield image
+        with Image(file=resource, resolution=300) as pdf_image:
+            # pdf_image.depth = 8
+            yield pdf_image
+
+
+@contextmanager
+def get_pdf_page_image(pdf_image, index):
+    """ We could have used `wand.Image.sequence` but it pushes all pages into memory.
+    This is the same code but only yields one page at a time so it's more efficient. """
+    wand = pdf_image.wand
+    image = library.GetImageFromMagickWand(wand)
+    exc = libmagick.AcquireExceptionInfo()
+    single_image = libmagick.CloneImages(image, binary(str(index)), exc)
+    libmagick.DestroyExceptionInfo(exc)
+    single_wand = library.NewMagickWandFromImage(single_image)
+    single_image = libmagick.DestroyImage(single_image)
+    with SingleImage(single_wand, pdf_image, image) as _:
+        with Image(_) as page_image:
+            yield page_image
+    del single_image
+    del single_wand
+    del image
 
 
 def get_thumbnails(url: str) -> Iterator[Tuple[int, Image]]:
-    with get_image(url) as image:
-        for page, thumbnail in enumerate(image.sequence):
-            with Image(thumbnail, resolution=288) as thumbnail:
-                thumbnail.background_color = Color("#fff")
-                thumbnail.alpha_channel = "remove"
-                width = min(thumbnail.width, 4_096)
-                thumbnail.transform(resize=f"{width}x")
-                yield page, thumbnail
+    """ Iterate through pages of a pdf, rasterize and resize, yielding `wand.image.Image`
+    objects.
+
+    """
+    with get_pdf_image(url) as pdf_image:
+        library.MagickResetIterator(pdf_image.wand)
+        page = 0
+        while True:
+            if not library.MagickNextImage(pdf_image.wand):
+                break
+            with get_pdf_page_image(pdf_image, page) as page_image:
+                page_image.background_color = Color("#fff")
+                page_image.alpha_channel = "remove"
+                page_image.transform(resize=f"2000x")
+                yield page, page_image
+                from wand.display import display
+
+                display(Image(blob=page_image.make_blob("PNG"), format="PNG"))
+                page += 1
+
+
+def _update_attachment_with_pdf_thumbnails(attachment: Attachment, db: Session):
+    """ Have a cleaner version of this function for when calling directly, debugging,
+    testing etc.
+    """
+    if attachment.type != "application/pdf":
+        return
+    for page, thumbnail in get_thumbnails(attachment.url):
+        key = f"{ENV}/attachments/{attachment.id}/{page}.png"
+        bytes_io = BytesIO(thumbnail.make_blob("PNG"))
+        upload(bytes_io, key)
+        bytes_io.close()
+        db.add(AttachmentThumbnail(attachment=attachment, bucket=BUCKET_NAME, key=key))
+    db.commit()
 
 
 @dramatiq.actor
@@ -52,12 +105,4 @@ def update_attachment_with_pdf_thumbnails(id: int) -> None:
         attachment = db.query(Attachment).get(id)
         if attachment is None:
             return
-        if attachment.type != "application/pdf":
-            return
-        for page, thumbnail in get_thumbnails(attachment.url):
-            key = f"{ENV}/attachments/{attachment.id}/{page}.png"
-            upload(BytesIO(thumbnail.make_blob("PNG")), key)
-            db.add(
-                AttachmentThumbnail(attachment=attachment, bucket=BUCKET_NAME, key=key)
-            )
-        db.commit()
+        _update_attachment_with_pdf_thumbnails(attachment, db)
