83d8fe291422aad8eb9066569993efdb82a7a0ea
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-08-10 13:48:25 +0000

    aws-ses config header in smtp send-email

--- planitor/endpoints/mail.py
+++ planitor/endpoints/mail.py
@@ -21,6 +21,7 @@ def send_email(email_to: str, subject: str, html_template: str, context: dict):
         subject=JinjaTemplate(subject),
         html=jinja_env.get_template(html_template).render(context),
         mail_from=(config("EMAILS_FROM_NAME"), config("EMAILS_FROM_EMAIL")),
+        headers={"X-SES-CONFIGURATION-SET": "planitor-ses-configuration"},
     )
     smtp_options = {"host": config("SMTP_HOST"), "port": config("SMTP_PORT", cast=int)}
     smtp_options["tls"] = config("SMTP_TLS", cast=bool, default=True)
