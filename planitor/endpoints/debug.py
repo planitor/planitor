432a573b59fae2ba109e50fdd9dbcf075f7bcd8a
Jökull Sólberg Auðunsson <jokull@solberg.is>
2020-09-10 08:38:23 +0000

    Cleanup

--- planitor/endpoints/debug.py
+++ planitor/endpoints/debug.py
@@ -1,49 +1,11 @@
-from itertools import groupby
-
 from fastapi import APIRouter, Depends, Response
-from sqlalchemy import func
 from sqlalchemy.orm import Session
 from planitor.mail import get_html
 
 from planitor.database import get_db
-from planitor.models import (
-    Entity,
-    Subscription,
-    User,
-    Case,
-    Minute,
-    Delivery,
-    SubscriptionTypeEnum,
-)
 from planitor.monitor import iter_user_meeting_deliveries, iter_user_weekly_deliveries
 
 router = APIRouter()
-
-
-@router.get("/mail/subscription_immediate.html")
-def _mail_debug(db: Session = Depends(get_db)) -> Response:
-    user = db.query(User).order_by(func.random()).first()
-    minute = (
-        db.query(Minute)
-        .join(Case)
-        .filter(Case.iceaddr != None)
-        .order_by(func.random())
-        .first()
-    )
-    delivery = Delivery(
-        subscription=Subscription(
-            user=user,
-            type=SubscriptionTypeEnum.radius,
-            radius=200,
-            address=minute.case.iceaddr,
-        ),
-        minute=minute,
-    )
-    html = get_html(
-        "subscription_immediate.html",
-        {"delivery": delivery, "email_to": "jokull@solberg.is"},
-    )
-    return Response(html, media_type="text/html")
 
 
 @router.get("/mail/subscription_weekly.html")
